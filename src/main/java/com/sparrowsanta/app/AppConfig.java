9c4895b638c836fd69450b4f1c38178d6f29e5a4
Sahtrok <mikolajczakmar@gmail.com>
2020-06-15 10:07:55 +0200

    Poprawione: AppConfig i AppInitializer (nie implementowały interfacow)
    Dodano:
    -AppConfig
    Ustawienia dla in18
    Ustawienia dla resourcow statycznych
    
    -Controlery
    login
    
    -Css
    css dla login
    
    -Logback
    xml dla logbacka z konfiguracją
    klasy LazyFileAppender i LazyFileOutputStream (implementacja tworząca plik tylko kiedy jest logowana jakaś zawartość)
    
    -i18n
    pliki z tłumaczeniami en i pl
    
    -images
    obrazki dla strony logowania

--- src/main/java/com/sparrowsanta/app/AppConfig.java
+++ src/main/java/com/sparrowsanta/app/AppConfig.java
@@ -1,11 +1,65 @@
 package com.sparrowsanta.app;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.sparrowsanta.controles")
-public class AppConfig {
+@ComponentScan(basePackages = "com.sparrowsanta.controllers")
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/views/", ".jsp");
+    }
+
+    //Ustawienie dla i18n - mapowanie resourców z plikami tłumaczeń
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    //Ustawienie dla i18n - definicja ciasteczka dla obsługi języków i defaultowego stanu
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en"));
+        resolver.setCookieName("myLangCookie");
+        resolver.setCookieMaxAge(60*60*24*365); //Czas życia - rok
+        return resolver;
+    }
+    //Ustawienie dla i18n - definicja nazwy dla parametru zmiany języka
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+
+    //Ustawienie mapowania dla zasobów statycznych
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/images/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
+
+    //Serwowanie statycznych zasobów
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+
 }
---
+++ src/main/java/com/sparrowsanta/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package com.sparrowsanta.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @GetMapping({"/", "/login"})
+    public String login() {
+        logger.info("Test loggera");
+        logger.error("Test loggera file");
+        return "login";
+
+    }
+
+}
