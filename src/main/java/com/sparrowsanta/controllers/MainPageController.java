ed79ab67eaf3d6552fffabd222a32bf9052e72c3
Sahtrok <mikolajczakmar@gmail.com>
2020-06-15 20:38:43 +0200

    Test4

--- src/main/java/com/sparrowsanta/controllers/MainPageController.java
+++ src/main/java/com/sparrowsanta/controllers/MainPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainPageController {
 
-    @GetMapping("/mainpage")
+    @GetMapping({"/mainpage","/welcome"})
     public String getMainPage(){
         return "mainpage";
     }
